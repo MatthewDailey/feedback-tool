a2d0ccc4210d69648568afe37a262a238990d764
Matt Dailey <mjd@figma.com>
2020-11-30 12:40:10 -0800

    working auth example

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,56 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { Firebase } from './firebase'
+import './firebase'
 import firebase from "firebase"
+import { createStore, combineReducers } from 'redux'
+import { Provider, useSelector } from 'react-redux'
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer,
+  useFirebase
+} from 'react-redux-firebase'
 
 const GoogleAuthProvider = new firebase.auth.GoogleAuthProvider()
 
 type Content = string
 
-const message: Content = 'hidy pals'
+const message: Content = '1'
 
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer
+  // firestore: firestoreReducer // <- needed if using firestore
+})
+// Create store with reducers and initial state
+const initialState = {}
+const store = createStore(rootReducer, initialState)
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users'
+  // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+}
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch
+  // createFirestoreInstance // <- needed if using firestore
+}
 
-const LoginButton = () => <button onClick={() => Firebase.auth().signInWithPopup(GoogleAuthProvider)}>Log In</button>
-const LogoutButton = () => <button onClick={() => Firebase.auth().signOut()}>Log out</button>
+const LoginButton = () => {
+  const firebase = useFirebase()
+  return <button onClick={() => firebase.login({ provider: 'google', type: 'popup' })}>Log In</button>
+}
+const LogoutButton = () => {
+  const firebase = useFirebase()
+  return <button onClick={() => firebase.logout()}>Log out</button>
+}
 
 const AppView = () => {
-  React.useEffect(() => Firebase.auth().onAuthStateChanged(console.log))
+  const profile = useSelector(state => state.firebase.auth)
 
   return (
     <div>
-      <h1>{`${message}`}</h1>
+      <p>{`${message} ${JSON.stringify(profile)}`}</p>
       <LoginButton />
       <br/>
       <LogoutButton />
@@ -26,7 +58,15 @@ const AppView = () => {
   )
 }
 
+const App = () => (
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <AppView />
+    </ReactReduxFirebaseProvider>
+  </Provider>
+)
+
 ReactDOM.render(
-  <AppView />,
+  <App />,
   document.getElementById('root')
 );
