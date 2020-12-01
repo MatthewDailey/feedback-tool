8dcfc3b2ec4f3d4407822c04eef29a07ec85795e
Matt Dailey <mjd@figma.com>
2020-12-01 11:40:16 -0800

    facotr out firebase set up

--- src/app.tsx
+++ src/app.tsx
@@ -1,47 +1,14 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import './firebase'
-import firebase from "firebase"
-import { createStore, combineReducers } from 'redux'
-import { Provider, useSelector } from 'react-redux'
-import {
-  ReactReduxFirebaseProvider,
-  firebaseReducer,
-  useFirebase
-} from 'react-redux-firebase'
+import { reactReduxFirebaseProps, store } from './firebase'
+import { Provider, } from 'react-redux'
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
 import {
   BrowserRouter,
   Switch,
   Route,
 } from "react-router-dom";
 import { Header } from "./header"
-
-type Content = string
-
-const message: Content = '1'
-
-// Add firebase to reducers
-const rootReducer = combineReducers({
-  firebase: firebaseReducer
-  // firestore: firestoreReducer // <- needed if using firestore
-})
-// Create store with reducers and initial state
-const initialState = {}
-const store = createStore(rootReducer, initialState)
-// react-redux-firebase config
-const rrfConfig = {
-  userProfile: 'users'
-  // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
-}
-const rrfProps = {
-  firebase,
-  config: rrfConfig,
-  dispatch: store.dispatch
-  // createFirestoreInstance // <- needed if using firestore
-}
-
-// @ts-ignore
-window.store = store
 
 const AppView = () => {
 
@@ -70,7 +37,7 @@ const Router = () => (
 
 const App = () => (
   <Provider store={store}>
-    <ReactReduxFirebaseProvider {...rrfProps}>
+    <ReactReduxFirebaseProvider {...reactReduxFirebaseProps}>
       <Router />
     </ReactReduxFirebaseProvider>
   </Provider>
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,35 @@
 import firebase from "firebase/app"
 import "firebase/auth"
+import "firebase/database"
 import { firebaseConfig } from "./firebase_config_dev"
+import { combineReducers, createStore } from "redux"
+import { firebaseReducer } from "react-redux-firebase"
 
-export const Firebase = firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig)
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer
+  // firestore: firestoreReducer // <- needed if using firestore
+})
+// Create store with reducers and initial state
+const initialState = {}
+export const store = createStore(rootReducer, initialState)
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users'
+  // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+}
+export const reactReduxFirebaseProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch
+  // createFirestoreInstance // <- needed if using firestore
+}
+
+// @ts-ignore
+window.store = store
+
+
+
