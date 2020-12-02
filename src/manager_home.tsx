2d126b9a9730acb7267d6836304996785d5a9739
Matt Dailey <mjd@figma.com>
2020-12-02 11:21:08 -0800

    introduce loading feedback session request

--- src/manager_home.tsx
+++ src/manager_home.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { useSelector } from 'react-redux'
 import {
   Link
 } from "react-router-dom";
--- src/participant.tsx
+++ src/participant.tsx
@@ -1,8 +1,38 @@
 import * as React from 'react'
 import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { isEmpty, isLoaded, useFirebaseConnect } from "react-redux-firebase"
+import { Contact } from "./auth"
+
+type FeedbackSessionRequest = {
+  sessionId: string,
+  sessionName: string,
+  sessionOwnerName: string,
+  sessionOwnerEmail: string,
+  requesteeName: string,
+  requesteeEmail: string,
+  participants: Contact[],
+  responseEmails: string[],
+}
+type FeedbackSessionRequestLoad = { loaded: false } | { loaded: true, request: null|FeedbackSessionRequest}
+
+const useFeedbackSessionRequest = (id: string) : FeedbackSessionRequestLoad => {
+  useFirebaseConnect([
+    { path: `feedbackSessionRequests/${id}` }
+  ])
+  const feedbackSessionRequests = useSelector(state => state.firebase.data.feedbackSessionRequests)
+  if (!isLoaded(feedbackSessionRequests)) {
+    return { loaded: false }
+  }
+  if (isEmpty(feedbackSessionRequests[id])) {
+    return { loaded: true, request: null }
+  }
+  return { loaded: true, request: feedbackSessionRequests[id]}
+}
 
 export const Participant = () => {
   const { feedbackSessionRequestId }  = useParams()
-  console.log("hi" + feedbackSessionRequestId)
-  return <div>{"request id: " + feedbackSessionRequestId}</div>
+  const feedbackSessionRequest = useFeedbackSessionRequest(feedbackSessionRequestId)
+
+  return <div>{"request id: " + JSON.stringify(feedbackSessionRequest)}</div>
 }
