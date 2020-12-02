db385309dbaba15f9c97386ccf88ed3b6bfca8ae
Matt Dailey <mjd@figma.com>
2020-12-02 14:41:09 -0800

    add functions with basic sendgrid email that doesn't work yet bc of domain verification

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,21 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import * as SendGrid from '@sendgrid/mail'
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+SendGrid.setApiKey('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')
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
+
+  const msg = {
+    to: 'matthew.j.dailey@gmail.com', // Change to your recipient
+    from: 'santa@feedback.gifts', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+  SendGrid.send(msg)
+    .then(() => functions.logger.info('Sent email!'))
+    .catch(e => functions.logger.error('Error sending email', e))
+
   response.send("Hello from Firebase!");
 });
