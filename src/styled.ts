a9d7f5159110b63e3f24069e97b8e91b0430abf6
Matt Dailey <mjd@figma.com>
2021-01-17 13:03:16 -0800

    add styles to text input

---
+++ src/styled.ts
@@ -0,0 +1,16 @@
+import { createStyled } from "@stitches/react"
+
+export const { styled, css } = createStyled({
+  prefix: '',
+  tokens: {
+    colors: {
+      $light: '#FAF9F9',
+      $dark: '#555B6E',
+      $seafoam: '#BEE3DB',
+      $teal: '#89B0AE',
+      $peach: '#FFD6BA',
+    }
+  },
+  breakpoints: {},
+  utils: {},
+});
--- src/text_input.tsx
+++ src/text_input.tsx
@@ -1,5 +1,39 @@
 import * as React from 'react'
+import { styled } from './styled'
 
+const StyledInput = styled('input', {
+  border: '1px solid $dark',
+
+  '&:focus': {
+    border: '1px solid $teal',
+  },
+
+  variants: {
+    inputSize: {
+      large: {
+        height: '40px',
+        width: '300px',
+        'border-radius': '8px',
+        padding: '12px 16px',
+      },
+      small: {
+        height: '24px',
+        'border-radius': '4px',
+        padding: '4px 8px',
+      },
+    },
+  }
+})
+
+const Container = styled('div', {
+  display: 'flex',
+  'flex-direction': 'column',
+  'align-items': 'start',
+})
+
+const Label = styled('label', {
+  'font-size': '8px'
+})
 
 export const TextInput = (props: {
   label?: string,
@@ -10,15 +44,16 @@ export const TextInput = (props: {
 }) => {
   const inputId = `${props.label}-input`
   return (
-    <div className={`${props.size} TextInput`}>
-      {props.label && <label htmlFor={inputId}>{props.label}</label>}
-      <input
+    <Container>
+      {props.label && <Label htmlFor={inputId}>{props.label}</Label>}
+      <StyledInput
         id={inputId}
         type="text"
         value={props.value}
         placeholder={props.hint}
         onChange={(e) => props.onChange(e.target.value)}
+        inputSize={props.size}
       />
-    </div>
+    </Container>
   )
 }
